76be42fbdbe5790b445e55d6e417ec78f5b12490
dodzikojo <dodzi@windowslive.com>
2020-05-18 15:33:30 +0100

    first commit

---
+++ readClashReport/MainWindow.xaml.cs
@@ -0,0 +1,296 @@
+﻿using Microsoft.Win32;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Windows;
+using System.Windows.Input;
+using System.Windows.Navigation;
+using Ookii.Dialogs.Wpf;
+using System.IO;
+using System.Threading.Tasks;
+using System.Windows.Data;
+
+namespace readClashReport
+{
+    public class htmlFiles
+    {
+        public string filename { get; set; }
+        public string clashes { get; set; }
+        public string newClashes { get; set; }
+        public string active { get; set; }
+        public string reviewed { get; set; }
+        public string type { get; set; }
+
+    }
+    
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        private static string filename { get; set; }
+        public static List<htmlFiles> fileData = new List<htmlFiles>();
+        string[] filePaths;
+        List<string> filePathList = new List<string>();
+        public MainWindow()
+        {
+            
+            InitializeComponent();
+
+            try
+            {
+                if (Properties.Appsettings.Default.filePathSetting.Length > 0)
+                {
+                    this.folderTxtBox.Text = Properties.Appsettings.Default.filePathSetting;
+                    filePaths = Directory.GetFiles(this.folderTxtBox.Text, "*.html");
+
+                    //List<htmlFiles> fileData = new List<htmlFiles>();
+                    this.countLabel.Content = filePaths.Length.ToString();
+
+                    if (isValid(filePaths))
+                    {
+                        DisplayData();
+                    }
+                    
+
+                }
+            }
+            catch (Exception e)
+            {
+
+                Debug.WriteLine(e.Message);
+            }
+                
+                
+            
+            
+            
+        }
+
+       
+
+        private void webBrowser_Nav(object sender, NavigatingCancelEventArgs e)
+        {
+            //if (!willNavigate)
+            //{
+            //    willNavigate = true;
+            //    return;
+            //}
+
+            e.Cancel = false;
+
+            //Process.Start(e.Uri.ToString());
+
+            try
+            {
+                var startInfo = new ProcessStartInfo
+                {
+                    FileName = e.Uri.ToString()
+                };
+
+                Process.Start(e.Uri.OriginalString); 
+            }
+            catch (Exception ex2)
+            {
+
+                Debug.WriteLine(ex2.Message);
+            }
+            
+            //Debug.WriteLine(e.Uri.ToString());
+            //Debug.WriteLine(e.Uri.OriginalString);
+        }
+
+        private void webBrowser_Navigated(object sender, NavigationEventArgs e)
+        {
+            Debug.WriteLine(e.Uri.ToString());
+        }
+
+        private void webBrowser_LoadCompleted(object sender, NavigationEventArgs e)
+        {
+           
+        }
+
+        private  void browseBtn_Click(object sender, RoutedEventArgs e)
+        {
+            bool valid = false;
+            VistaFolderBrowserDialog abcd = new VistaFolderBrowserDialog();
+            Nullable<bool> sample = abcd.ShowDialog();
+            try
+            {
+                //bool valid;
+                if (sample == true )
+                {
+                    string[] tempFilepaths;
+                    this.folderTxtBox.Text = abcd.SelectedPath.ToString();
+                    tempFilepaths = Directory.GetFiles(abcd.SelectedPath.ToString(), "*.html");
+                    if (tempFilepaths.Length > 0)
+                    {
+                        fileData.Clear();
+                        valid = isValid(tempFilepaths);
+                        
+                        
+
+
+                        //List<htmlFiles> fileData = new List<htmlFiles>();
+                        this.countLabel.Content = filePathList.Count.ToString();
+                        
+                    }
+
+                    if (valid)
+                    {
+                        fileData.Clear();
+                        DisplayData();
+                        Properties.Appsettings.Default.filePathSetting = abcd.SelectedPath.ToString();
+                    }
+                    
+
+
+                }
+            }
+            catch (Exception ex1)
+            {
+
+                Debug.WriteLine(ex1.Message);
+            }
+           
+
+            
+           
+
+        }
+
+        public bool isValid(string[] tempFilepaths)
+        {
+            bool valid;
+            filePathList.Clear();
+            if (tempFilepaths.Length > 0)
+            {
+                foreach (string item in tempFilepaths)
+                {
+                    if (Path.GetFileNameWithoutExtension(item).ToLower().Contains("vs"))
+                    {
+                        filePathList.Add(item);
+                    }
+                }
+                valid = true;
+            }
+            else
+            {
+                valid = false;
+                MessageBox.Show("Error","No valid files found.");
+            }
+            return valid;
+        }
+
+        public async void DisplayData()
+        {
+            filesListView.ItemsSource = null;
+            filesListView.Items.Clear();
+            fileData.Clear();
+
+            await Task.Run(() =>
+            {
+                try
+                {
+                    foreach (string html in filePathList)
+                    {
+                        readHTML.readHTMLData(@html);
+                        //fileData.Add(new htmlFiles() { filename = "lajlfaa" });
+
+                        this.Dispatcher.Invoke(() =>
+                        {
+                            filesListView.ItemsSource = fileData;
+                            filesListView.Items.Refresh();
+
+                        });
+                    }
+                    this.Dispatcher.Invoke(() =>
+                    {
+                        this.webViewer.Navigate(filePathList[0]);
+
+                    });
+
+                }
+                catch (Exception ex)
+                {
+
+                    Debug.WriteLine(ex.Message);
+                }
+
+                
+
+            });
+
+            try
+            {
+                CollectionView view = (CollectionView)CollectionViewSource.GetDefaultView(filesListView.ItemsSource);
+                view.Filter = UserFilter;
+            }
+            catch (Exception e)
+            {
+
+                Debug.WriteLine(e.Message);
+            }
+            
+        }
+        private bool UserFilter(object item)
+        {
+            if (String.IsNullOrEmpty(txtFilter.Text))
+                return true;
+            else
+                return ((item as htmlFiles).filename.IndexOf(txtFilter.Text, StringComparison.OrdinalIgnoreCase) >= 0);
+        }
+
+        private void txtFilter_TextChanged(object sender, System.Windows.Controls.TextChangedEventArgs e)
+        {
+            CollectionViewSource.GetDefaultView(filesListView.ItemsSource).Refresh();
+        }
+
+
+        private void filesListView_MouseDoubleClick(object sender, MouseButtonEventArgs e)
+        {
+            //System.Collections.IList<string> abcd =  this.filesListView.SelectedItems;
+
+            foreach (htmlFiles item in this.filesListView.SelectedItems)
+            {
+                Debug.WriteLine(Path.Combine(this.folderTxtBox.Text, item.filename+".html"));
+                this.webViewer.Navigate(Path.Combine(this.folderTxtBox.Text, item.filename + ".html"));
+            }
+
+            //if (this.filesListView.SelectedIndex < 0)
+            //{
+
+            //}
+            //else
+            //{
+            //    this.webViewer.Navigate(filePaths[this.filesListView.SelectedIndex]);
+            //    //Path.Combine(this.folderTxtBox.Text, item.filename);
+            //}
+            
+
+            //Debug.WriteLine(filesListView.SelectedIndex.ToString());
+        }
+
+        private void Window_Closing(object sender, System.ComponentModel.CancelEventArgs e)
+        {
+            Properties.Appsettings.Default.Save();
+            e.Cancel = true;
+            //this.Visibility = Visibility.Visible;
+            try
+            {
+                this.Close();
+            }
+            catch (Exception ex)
+            {
+                Environment.Exit(0);
+                Debug.WriteLine(ex.Message);
+            }
+            
+        }
+
+        //private void wbSample_Navigating(object sender, System.Windows.Navigation.NavigatingCancelEventArgs e)
+        //{
+        //    Debug.WriteLine(e.Uri.OriginalString);
+        //}
+    }
+}
---
+++ readClashReport/readHTML.cs
@@ -0,0 +1,166 @@
+﻿using HtmlAgilityPack;
+using System;
+using System.Diagnostics;
+using System.Collections.Generic;
+using System.Text;
+using System.IO;
+
+namespace readClashReport
+{
+    class readHTML
+    {
+        private static int clashesCounter { get; set; }
+        private static int toleranceCounter { get; set; }
+        private static int newcounter { get; set; }
+        private static int reviewedCounter { get; set; }
+        private static int approvedCounter { get; set; }
+        private static int activeCounter { get; set; }
+        private static int resolvedCounter { get; set; }
+        private static int typeCounter { get; set; }
+
+        public static htmlFiles fileData { get; set; }
+
+        //private static Stopwatch watch;
+        public static void readHTMLData(string html)
+        {
+            var htmlDoc = new HtmlDocument();
+            htmlDoc.Load(html);
+            //htmlDoc.LoadHtml(html);
+
+
+            var htmlNodes = htmlDoc.DocumentNode.SelectNodes("//body/table");
+           // MainWindow.fileData.Add(new htmlFiles() { filename = html });
+
+            toleranceCounter = 0;
+            clashesCounter = 0;
+            newcounter = 0;
+            reviewedCounter = 0;
+            approvedCounter = 0;
+            resolvedCounter = 0;
+            typeCounter = 0;
+            activeCounter = 0;
+
+            string filenameTemp = string.Empty;
+            string clashesCounttemp = string.Empty;
+            string activeTemp = string.Empty;
+            string newTemp = string.Empty;
+            string typeTemp = string.Empty;
+            string toleranceTemp = string.Empty;
+            string resolvedTemp = string.Empty;
+
+            foreach (var node in htmlNodes)
+            {
+                if (node.HasClass("testSummaryTable"))
+                {
+                    var abc = node.Descendants();
+
+                    foreach (var nodes in abc)
+                    {
+
+                        if (nodes.HasClass("headerCell"))
+                        {
+                            toleranceCounter++;
+                            if (nodes.InnerText.ToString() == "Tolerance")
+                            {
+                                var nodess = htmlDoc.DocumentNode.SelectSingleNode($"(//body/table[2]/tr[@class='contentRow'])/td[{toleranceCounter}]");
+                                Debug.WriteLine($"Tolerance is: {nodess.InnerText.ToString()}");
+                                
+                            }
+                        }
+
+
+                        if (nodes.HasClass("headerCell"))
+                        {
+                            clashesCounter++;
+                            if (nodes.InnerText.ToString() == "Clashes")
+                            {
+                                var nodess = htmlDoc.DocumentNode.SelectSingleNode($"(//body/table[2]/tr[@class='contentRow'])/td[{clashesCounter}]");
+                                Debug.WriteLine($"Total number of clashes are: {nodess.InnerText.ToString()}");
+                                clashesCounttemp = nodess.InnerText.ToString();
+                                //filenameTemp = html;
+                                //MainWindow.fileData.Add(new htmlFiles() { clashes = nodess.InnerText.ToString(), filename = html });
+                            }
+                        }
+
+                        if (nodes.HasClass("headerCell"))
+                        {
+                            newcounter++;
+                            if (nodes.InnerText.ToString() == "New")
+                            {
+                                var nodess = htmlDoc.DocumentNode.SelectSingleNode($"(//body/table[2]/tr[@class='contentRow'])/td[{newcounter}]");
+                                //MainWindow.fileData.Add(new htmlFiles() { newClashes = nodess.InnerText.ToString()});
+                                newTemp = nodess.InnerText.ToString();
+                                //Debug.WriteLine($"Number of new clashes: {nodess.InnerText.ToString()}");
+                            }
+                        }
+
+                        if (nodes.HasClass("headerCell"))
+                        {
+                            activeCounter++;
+                            if (nodes.InnerText.ToString() == "Active")
+                            {
+                                var nodess = htmlDoc.DocumentNode.SelectSingleNode($"(//body/table[2]/tr[@class='contentRow'])/td[{activeCounter}]");
+                                activeTemp = nodess.InnerText.ToString();
+                                //Debug.WriteLine($"Number of active clashes: {nodess.InnerText.ToString()}");
+                            }
+                        }
+
+                        if (nodes.HasClass("headerCell"))
+                        {
+                            reviewedCounter++;
+                            if (nodes.InnerText.ToString() == "Reviewed")
+                            {
+                                var nodess = htmlDoc.DocumentNode.SelectSingleNode($"(//body/table[2]/tr[@class='contentRow'])/td[{reviewedCounter}]");
+                                
+                                //Debug.WriteLine($"Number of reviewed clashes: {nodess.InnerText.ToString()}");
+
+                            }
+                        }
+
+                        if (nodes.HasClass("headerCell"))
+                        {
+                            approvedCounter++;
+                            if (nodes.InnerText.ToString() == "Approved")
+                            {
+                                var nodess = htmlDoc.DocumentNode.SelectSingleNode($"(//body/table[2]/tr[@class='contentRow'])/td[{approvedCounter}]");
+                                //Debug.WriteLine($"Number of approved clashes: {nodess.InnerText.ToString()}");
+                            }
+                        }
+
+                        if (nodes.HasClass("headerCell"))
+                        {
+                            resolvedCounter++;
+                            if (nodes.InnerText.ToString() == "Resolved")
+                            {
+                                var nodess = htmlDoc.DocumentNode.SelectSingleNode($"(//body/table[2]/tr[@class='contentRow'])/td[{resolvedCounter}]");
+                                //Debug.WriteLine($"Number of resolved clashes: {nodess.InnerText.ToString()}");
+                            }
+                        }
+
+                        if (nodes.HasClass("headerCell"))
+                        {
+                            typeCounter++;
+                            if (nodes.InnerText.ToString() == "Type")
+                            {
+                                var nodess = htmlDoc.DocumentNode.SelectSingleNode($"(//body/table[2]/tr[@class='contentRow'])/td[{typeCounter}]");
+                                //Debug.WriteLine($"Type of clash performed: {nodess.InnerText.ToString()}");
+                                //Debug.WriteLine("\n");
+                            }
+                        }
+
+                    }
+                    MainWindow.fileData.Add(new htmlFiles() { clashes = clashesCounttemp, filename = Path.GetFileNameWithoutExtension(html), newClashes = newTemp });
+
+                }
+
+            }
+
+
+
+
+            //int num1 = 2;
+
+
+        }
+    }
+}
